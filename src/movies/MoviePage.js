02c745b52be50d782d181536a3b8a9822fe1019d
NickDayFSD <Nick.Day.FSD@gmail.com>
2021-05-20 12:04:39 -0700

    added favorite handler

--- src/movies/MoviePage.js
+++ src/movies/MoviePage.js
@@ -15,12 +15,15 @@ export default class MoviePage extends Component {
   async componentDidMount() {
     try {
       this.setState({ loading: true });
+
       const favorites = await getFavorites();
       this.setState({ favorites: favorites });
     }
+
     catch (err) {
       console.log(err.message);
     }
+
     finally {
       this.setState({ loading: false });
     }
@@ -29,31 +32,77 @@ export default class MoviePage extends Component {
   handleSearch = async search => {
     try {
       this.setState({ loading: true });
+
       const favorites = await getFavorites();
       const movies = await getMovies(search);
+
       const refinedMovies = movies.map(movie => {
         const found = favorites.find(favorite => favorite.movieId === movie.movieId);
         return found ? found : movie;
       });
+
       this.setState({ movies: refinedMovies });
     }
+
     catch (err) {
       console.log(err.message);
     }
+
     finally {
       this.setState({ loading: false });
     }
   }
 
+  handleFavorited = async movie => {
+    try {
+      this.setState({ loading: true });
+      const { movies } = this.state;
+
+      const favoriteId = movie.id;
+
+      if (favoriteId) {
+        await deleteFavorite(favoriteId);
+
+        const updateMovies = movies.map(movie => {
+          return movie.id === favoriteId
+            ? {
+              movieId: movie.movieId,
+              title: movie.title,
+              image: movie.img
+            }
+            : movie;
+        });
+
+        this.setState({ movies: updateMovies });
+
+      } else {
+        const addedFavorite = await addFavorites(movie);
+
+        const updateMovies = movies.map(movie => {
+          return movie.movieId === addedFavorite.movieId ? addedFavorite : movie;
+        });
+
+        this.setState({ movies: updateMovies });
+      }
+    }
+
+    catch (err) {
+      console.log(err.message);
+    }
+
+    finally {
+      this.setState({ loading: false });
+    }
+  }
 
   render() {
     const { movies, loading } = this.state;
-  
+
     return (
       <div className="MoviePage">
-        <Loader loading={loading}/>
-        <MovieSearch onSearch={this.handleSearch}/>
-        <MovieList movies={movies} />
+        <Loader loading={loading} />
+        <MovieSearch onSearch={this.handleSearch} />
+        <MovieList movies={movies} onFavorited={this.handleFavorited} />
       </div>
     );
   }
