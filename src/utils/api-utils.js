dc70dedbd65dc4191a7bd5e4275ba6ce439bc492
NickDayFSD <Nick.Day.FSD@gmail.com>
2021-05-20 10:20:10 -0700

    connected server and added api utils

---
+++ src/utils/api-utils.js
@@ -0,0 +1,70 @@
+import request from 'superagent';
+
+export async function getMovies(search) {
+
+  const response = await request
+    .get('/api/movies')
+    .query({ search: search })
+    .set('Authorization', window.localStorage.getItem('TOKEN'));
+
+  return response.body;
+}
+
+export async function getMovie(id) {
+  const response = await request
+    .get(`/api/movies/${id}`)
+    .set('Authorization', window.localStorage.getItem('TOKEN'));
+
+  return response.body;
+}
+
+export async function getFavorites() {
+  const response = await request
+    .get('/api/me/favorites')
+    .set('Authorization', window.localStorage.getItem('TOKEN'));
+
+  return response.body;
+}
+
+export async function addFavorites(favorite) {
+  const response = await request
+    .post('/api/favorites')
+    .send(favorite)
+    .set('Authorization', window.localStorage.getItem('TOKEN'));
+
+  return response.body;
+}
+
+export async function deleteFavorite(id) {
+  const response = await request
+    .delete(`/api/favorites/${id}`)
+    .set('Authorization', window.localStorage.getItem('TOKEN'));
+
+  return response.body;
+}
+
+export async function signUp(credentials) {
+  const response = await request
+    .post('/api/auth/signup')
+    .ok(res => res.status < 500)
+    .send(credentials);
+
+  if (response.status === 400) {
+    throw response.body;
+  }
+
+  return response.body
+}
+
+export async function signIn(credentials) {
+  const response = await request
+    .post('/api/auth/signin')
+    .ok(res => res.status < 500)
+    .send(credentials);
+
+  if (response.status === 400) {
+    throw response.body;
+  }
+
+  return response.body
+}
