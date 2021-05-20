64f12d483503b6367de627e27737a3a8c9e595fc
NickDayFSD <Nick.Day.FSD@gmail.com>
2021-05-19 17:10:09 -0700

    initialize auth

---
+++ src/auth/Auth.js
@@ -0,0 +1,14 @@
+import { Component } from 'react';
+import './Auth.css';
+
+export default class Auth extends Component {
+
+  render() {
+    return (
+      <div className="Auth">
+
+      </div>
+    );
+  }
+
+}
