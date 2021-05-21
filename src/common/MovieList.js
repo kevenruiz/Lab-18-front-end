3506c12e3ebae3c66e33b970091b892b937ad840
Annaleigh Thomas <annaleighxvx@gmail.com>
2021-05-20 20:32:28 -0400

    favorited

--- src/common/MovieList.js
+++ src/common/MovieList.js
@@ -5,12 +5,12 @@ import './MovieList.css';
 export default class MovieList extends Component {
 
   render() {
-    const { movies } = this.props;
+    const { movies, onFavorited } = this.props;
 
     return (
       <ul className="MovieList">
         {movies.map(movie => (
-          <MovieItem key={movie.movieId} movies={movie} />
+          <MovieItem key={movie.movieId} movies={movie} onFavorited={onFavorited}/>
         ))}
       </ul>
     );
